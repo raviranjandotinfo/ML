91621113513db5f64b885069d266d41e15f7e695
raviranjandotinfo <helloravi@live.com>
2024-07-14 11:01:02 -0400

    Update and rename HandWritting.py to libraries.py

---
+++ libraries.py
@@ -0,0 +1,29 @@
+
+# List of machine learning (ML) libraries that I have been using throughout the projects. 
+
+# NumPy :- Using for matrix, large multi-dimensional array and high-level mathematical functions. 
+import numpy as np 
+
+# SciPy :- Using for linear algebra, integration and statistics. 
+from scipy.misc import imread, imsave, imresize 
+
+# Scikit-learn :- It supports most of the supervised and unsupervised learning algorithms. Using for data-mining and data-analysis. 
+from sklearn import datasets 
+from sklearn import metrics 
+from sklearn.tree import DecisionTreeClassifier 
+from sklearn.preprocessing import MinMaxScaler
+
+# Theano :- Using it to define, evaluate and optimize mathematical expressions involving multi-dimensional arrays. 
+import theano 
+import theano.tensor as T 
+
+# TensorFlow :- Using it to train and run deep neural networks to develop several AI applications. 
+import tensorflow as tf 
+
+# PyTorch :- Used for Computer Vision, Natural Language Processing(NLP). 
+import torch
+
+# Pandas :- Used for data extraction, high-level data structures and data analysis. 
+import pandas as pd 
+
+# Matplotlib :- Used for graphs and plots for data visualization, histogram, error charts and bar chats.
